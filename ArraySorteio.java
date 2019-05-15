2e607a5efd3282aa32279b1466279ef90f8b09f4
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-05-14 21:21:37 -0300

    Update ArraySorteio.java

--- ArraySorteio.java
+++ ArraySorteio.java
@@ -8,23 +8,26 @@ public class Sorteio {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         System.out.println("Quantos participantes terão?");
-        int totalParticipantes = leia.nextInt();
-        int participante = 0;
-        String[] nomes = new String[totalParticipantes];
+        int totalParticipantes = leia.nextInt(); //Variavel total participante
+        int participante = 0; //Participante
+        String[] nomes = new String[totalParticipantes]; // Cria array nomes
         for(int i = 0; i < totalParticipantes; i++){
             participante = i + 1;
             System.out.println("Qual o nome do " + participante + "º participante");
-            nomes[i] = leia.next();
+            nomes[i] = leia.next(); //Pegar nome indice
             for(int b = 0; b < totalParticipantes; b++){
+                /* Verifica se é o indice menor que i */
                 if(b < i){
                 while(nomes[i].equals(nomes[b])){
                     System.out.println("Esse nome já existe tente novamente :) ");
                     System.out.println("Qual o nome do " + participante + "º participante");
                     nomes[i] = leia.next();
-                    }
+                    break;
+                }
                 }
             }
         }
+        /* Sorteia um número */
         Random r = new Random();
         int ganhador = r.nextInt(nomes.length);
         System.out.println("O ganhador é " + nomes[ganhador]);
