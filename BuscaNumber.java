d5ee64f223453287440bfc584f13a559a11f46ec
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-05-21 19:22:24 -0300

    Create BuscaNumber.java

---
+++ BuscaNumber.java
@@ -0,0 +1,35 @@
+package buscanumber;
+
+import java.util.Scanner;
+
+public class BuscaNumber {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int numberCount = 5;
+        int number[] = new int[numberCount];
+        boolean achei = false;
+        for(int i = 0; i < number.length; i++){
+            System.out.println("Escreva um numbero \nOu digite sair para fechar o programa");
+            number[i] = leia.nextInt();
+            int total = number.length - 1;
+            if(i >= total){
+                System.out.println("Legal cheguei ao número " + number.length + "\n Escreva um nome que talvez esteja na lista");
+                int numberBusca = leia.nextInt();
+                System.out.println("Olha oque eu achei :D");
+                for(int b = 0; b < number.length; b++){
+                    if(numberBusca == number[b]){
+                        achei = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if(achei){
+            System.out.println("Achei :D");
+        } else{
+            System.out.println("Não achei :C");
+        }
+    }
+    
+}
