aac61b8051eb943b03b4c2953e0392ddfbe37416
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-05-28 21:36:43 -0300

    Fix

--- Fatorial.java
+++ Fatorial.java
@@ -8,11 +8,12 @@ public class Fatorial {
         Scanner leia = new Scanner(System.in);
         System.out.println("Informeu um número");
         int number = leia.nextInt();
-        int fatorial = 1;
+        double fatorial = 1;
+        double percentual = 20;
         for(int i = number; i >= 1; i--){
             fatorial = fatorial * i;
         }
-        fatorial = (fatorial * 20) / 100;
+        fatorial = (fatorial * percentual) / 100;
         System.out.println(fatorial);
     }
     
