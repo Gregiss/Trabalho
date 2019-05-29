269eb3e40a8184a0a609ec0e0587dfb66d670914
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-05-28 21:55:49 -0300

    Fix

--- Fatorial.java
+++ Fatorial.java
@@ -9,17 +9,13 @@ public class Fatorial {
         System.out.println("Informeu um número\n -------------------------");
         long number = leia.nextInt();
         long numeroCalculo = 1;
-        long menor = number;
         long percentual = 20;
         for(long i = 1; i <= number; i++){
             numeroCalculo = numeroCalculo * i;
         }
         System.out.print(number);
-        for(int i = 1; i <= number; i++){
-            menor--;
-            if(i < number){
-                System.out.print(" * " + menor + " ");
-            }
+        for(long i = number; i >= 1; i--){
+            System.out.print(" * " + i + " ");
         }
         System.out.print("\nFatorial: " + numeroCalculo + "\n");
         numeroCalculo = (numeroCalculo * percentual) / 100;
