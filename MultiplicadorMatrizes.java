67f36e6ffc3b1078538f36b4661e782f1e7eabac
thekaway404 <46573030+thekaway404@users.noreply.github.com>
2019-06-05 19:06:29 -0300

    Fix

--- MultiplicadorMatrizes.java
+++ MultiplicadorMatrizes.java
@@ -17,20 +17,17 @@ public class Matrizes {
         numeroX = leia.nextInt();
         int[][] matrizA = new int[linhas][colunas];
         int[][] matrizB = new int[linhas][colunas];
-        
         for(int linha = 0; linha < matrizA.length; linha++){
             for(int coluna = 0; coluna < matrizA[linha].length; coluna++){
                 System.out.println("Informe um número para " + "[" + linha + "]" + "[" + coluna + "]=>");
                 matrizA[linha][coluna] = leia.nextInt();
             }
         }
-        
         for(int linha = 0; linha < matrizB.length; linha++){
             for(int coluna = 0; coluna < matrizB[linha].length; coluna++){
                 matrizB[linha][coluna] = numeroX * matrizA[linha][coluna];
             }
         }
-        
         for(int linha = 0; linha < matrizB.length; linha++){
             for(int coluna = 0; coluna < matrizB[linha].length; coluna++){
                 System.out.print(matrizB[linha][coluna] + "\t");
